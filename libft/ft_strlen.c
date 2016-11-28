9490d54ec0d60ad752c704d185c214bbfd6cdbbb
Cameron Fredrickson <cfredric@e1z2r2p8.42.us.org>
2016-11-28 13:19:01 -0800

    adding libft

---
+++ libft/ft_strlen.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfredric <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/21 10:40:55 by cfredric          #+#    #+#             */
+/*   Updated: 2016/09/21 10:40:56 by cfredric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	count;
+	int		index;
+
+	count = 0;
+	index = 0;
+	if (!str)
+		return (0);
+	while (str[index])
+	{
+		count++;
+		index++;
+	}
+	return (count);
+}
